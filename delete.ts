a57292bd9d89b5c23d004a7318dc299627e5045d
Jonny Burger <jonathanburger11@gmail.com>
2024-12-17 14:47:21 +0100

    Update delete.ts

--- delete.ts
+++ delete.ts
@@ -1,6 +1,5 @@
 import { getAwsClient, getOrCreateBucket, getRegions } from "@remotion/lambda";
 import dotenv from "dotenv";
-import pLimit from "p-limit";
 import { getAccountCount } from "./src/helpers/get-account-count";
 import { setEnvForKey } from "./src/helpers/set-env-for-key";
 
@@ -9,10 +8,12 @@ dotenv.config();
 const count = getAccountCount();
 console.log(`Found ${count} accounts. Deploying...`);
 
-const limit = pLimit(7);
-
 for (let i = 2; i <= count; i++) {
   for (const region of getRegions()) {
+    if (region === "us-west-1") {
+      continue;
+    }
+
     setEnvForKey(i);
     const { bucketName } = await getOrCreateBucket({ region });
     const { sdk, client } = getAwsClient({ region, service: "s3" });
@@ -28,45 +29,45 @@ for (let i = 2; i <= count; i++) {
           ContinuationToken: contToken,
         }),
       );
-      results.push(...(res.Contents ?? []));
-      console.log(
-        "Found",
-        results.length,
-        "objects in",
-        bucketName,
-        "in",
-        region,
-        "for account",
-        i,
-      );
+      if ((res.Contents?.length ?? 0) > 0) {
+        await client.send(
+          new sdk.DeleteObjectsCommand({
+            Bucket: bucketName,
+            Delete: {
+              Objects: res.Contents?.map((c) => ({
+                Key: c.Key as string,
+              })),
+            },
+          }),
+        );
+
+        results.push(...(res.Contents ?? []));
+        console.log(
+          "Deleted",
+          results.length,
+          "objects in",
+          bucketName,
+          "in",
+          region,
+          "for account",
+          i,
+        );
+      } else {
+        console.log(
+          "No more objects to delete in",
+          bucketName,
+          "in",
+          region,
+          "for account",
+          i,
+        );
+      }
+
       if (!res.NextContinuationToken) {
         break;
       }
 
       contToken = res.NextContinuationToken;
     }
-
-    let deleted = 0;
-
-    await Promise.all(
-      results?.map((c) => {
-        return limit(() =>
-          client
-            .send(
-              new sdk.DeleteObjectCommand({
-                Key: c.Key as string,
-                Bucket: bucketName,
-              }),
-            )
-            .then(() => {
-              console.log(
-                `((${++deleted}/${results.length}) Deleted ${
-                  c.Key
-                } in ${bucketName} (region)${region} for account ${i}`,
-              );
-            }),
-        );
-      }) ?? [],
-    );
   }
 }
